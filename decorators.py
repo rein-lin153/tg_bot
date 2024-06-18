1ee4ea70fe0c4064a06f38a0e810a177038c5bd7
rein-lin153 <l1531556074@gmail.com>
2024-06-19 01:53:31 +0800

    捕获ReadTimeout重试机制

--- decorators.py
+++ decorators.py
@@ -22,5 +22,11 @@ def handle_telegram_exception(retries=5, delay=5):
                         time.sleep(delay)  # 等待一段时间后重试
                     else:
                         raise  
+                except requests.exceptions.ReadTimeout as e:  # 捕获ReadTimeout异常
+                   bot.send_message(chat_id, "请求超时,尝试重试...")
+                   attempts += 1
+                   time.sleep(delay)  # 等待一段时间后重试
+            if attempts >= retries:  # 如果达到最大重试次数
+                bot.send_message(chat_id, "请求失败，请稍后再试。")  # 通知用户请求失败
         return wrapper
     return decorator
